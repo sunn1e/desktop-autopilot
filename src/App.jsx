10d38c5d4e0b4c964db21d3320bc8baf31983e29
dummy <sunny>
2022-11-26 00:43:09 +0530

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
@@ -11,34 +11,33 @@ function App() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
     setGreetMsg(await invoke("greet", { name }));
   }
-
+  async function launch() {
+    const win = window.open(
+      "https://app.sellergeni.com/login?autopilot=true",
+      "_self"
+    );
+    if (win != null) {
+      win.focus();
+    }
+  }
+  useEffect(() => {
+    launch();
+  }, []);
   return (
     <div className="container">
-      <h1>Welcome to Tauri!</h1>
+      <h1>Welcome to Autopilot Desktop!</h1>
 
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+      <p>Click on Launch to get started.</p>
 
       <div className="row">
         <div>
-          <input
-            id="greet-input"
-            onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Enter a name..."
-          />
-          <button type="button" onClick={() => greet()}>
-            Greet
+          <button
+            type="button"
+            onClick={() => {
+              launch();
+            }}
+          >
+            Launch
           </button>
         </div>
       </div>
